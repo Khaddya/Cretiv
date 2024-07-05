3278d1fd32182374a4fc02f8ea40e9587f497682
len-mendonca <lenmendonca2003@gmail.com>
2024-07-05 16:20:53 +0530

    chore: Update npm dependencies and add new packages
    - Update "@radix-ui/react-select" to version 2.1.1
    - Add new package "react-color" version 2.19.3
    - Add new package "react-rnd" version 10.4.11
    - Update "@types/react-color" to version 3.0.12

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,158 @@
 import "./App.css";
 import { Input } from "./components/ui/input";
-import { Label } from "./components/ui/label";
-import { ChangeEvent, useRef, useState, FC } from "react";
+import { ChangeEvent, useRef, useState, useEffect } from "react";
 import Papa from "papaparse";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "../src/components/ui/table";
 
-import Draggable, { DraggableEvent, DraggableData } from 'react-draggable';
-
+import { Rnd } from "react-rnd";
+import { SketchPicker, ColorResult, RGBColor } from "react-color";
 
 interface ParsedData {
   [key: string]: string;
 }
 
-interface Position {
-  x: number;
-  y: number;
+interface MetaDetails {
+  Name: string;
+  IsItalic: string;
+  IsBold: string;
+  Color: RGBColor;
+  Size: number;
 }
 
-interface Positions {
-  [key: string]: Position;
+interface VarContentData {
+  Metadetails: MetaDetails;
+  VarContent: string[];
+  Location: { X: number; Y: number };
 }
+
+interface VarContentProps {
+  data: VarContentData;
+}
+
+interface DraggableResizableDivProps {
+  id: string;
+  position: { x: number; y: number };
+  size: { width: number; height: number };
+  onUpdate: (
+    id: string,
+    position: { x: number; y: number },
+    size: { width: number; height: number }
+  ) => void;
+  fontFamily: string;
+  name: string;
+  fontColor: RGBColor;
+}
+
+interface DivData {
+  id: string;
+  position: { x: number; y: number };
+  size: { width: number; height: number };
+  header: string;
+  active: boolean;
+  fontFamily: string;
+  fontColor: RGBColor;
+  info: string[];
+}
+
+const DraggableResizableDiv: React.FC<DraggableResizableDivProps> = ({
+  id,
+  position,
+  size,
+  onUpdate,
+  fontFamily,
+  name,
+  fontColor,
+}) => {
+  const fontSize = Math.min(size.width, size.height) * 0.3;
+
+  return (
+    <Rnd
+      className="bg-slate-400 opacity-50 border-2 border-darkblue flex justify-start items-start cursor-move"
+      size={{ width: size.width, height: size.height }}
+      position={{ x: position.x, y: position.y }}
+      onDragStop={(e: any, d: any) => onUpdate(id, { x: d.x, y: d.y }, size)}
+      onResizeStop={(e, direction, ref, delta, newPosition) => {
+        onUpdate(id, newPosition, {
+          width: parseInt(ref.style.width, 10),
+          height: parseInt(ref.style.height, 10),
+        });
+      }}
+      bounds="parent"
+    >
+      <div
+        style={{
+          fontSize: `${fontSize}px`,
+          whiteSpace: "normal",
+          overflow: "hidden",
+          wordWrap: "break-word",
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "start",
+          width: "100%",
+          height: "100%",
+          textAlign: "start",
+          fontFamily: fontFamily,
+          color: `rgba(${fontColor.r}, ${fontColor.g}, ${fontColor.b}, ${fontColor.a})`,
+        }}
+      >
+        {name}
+      </div>
+    </Rnd>
+  );
+};
 
 function App() {
+  const [varContent, setVarContent] = useState<VarContentProps[]>([]);
   const [file, setFile] = useState<File | null>(null);
   const [data, setData] = useState<ParsedData[]>([]);
+  const [divs, setDivs] = useState<DivData[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleDataRequest = async () => {
+    const updatedData = divs.map((div) => ({
+      data: {
+        Metadetails: {
+          Name: div.header,
+          IsItalic: "false",
+          IsBold: "false",
+          Color: div.fontColor,
+          Size: Math.min(div.size.width, div.size.height) * 0.3,
+        },
+        VarContent: div.info, // Assuming content is header, modify as needed
+        Location: {
+          X: div.position.x,
+          Y: div.position.y,
+        },
+      },
+    }));
+
+    setVarContent(updatedData);
+  };
+
+  const handleImgFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  const updateDiv = (
+    id: string,
+    newPosition: { x: number; y: number },
+    newSize: { width: number; height: number }
+  ) => {
+    setDivs(
+      divs.map((div) =>
+        div.id === id ? { ...div, position: newPosition, size: newSize } : div
+      )
+    );
+  };
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -57,64 +177,185 @@ function App() {
     }
   };
 
-
-  const parentRef = useRef<HTMLDivElement>(null);
-  const [positions, setPositions] = useState<Positions>({});
-
-  const handleDragStop = (e: DraggableEvent, data: DraggableData, id: string) => {
-    if (parentRef.current) {
-      const parentRect = parentRef.current.getBoundingClientRect();
-      const childRect = data.node.getBoundingClientRect();
-      const x = childRect.left - parentRect.left;
-      const y = childRect.top - parentRect.top;
-
-      setPositions((prev) => ({
-        ...prev,
-        [id]: { x, y }
-      }));
-      console.log(positions);
+  const handleFinalSubmit = async () => {
+    if (!selectedFile) {
+      console.error("No file selected.");
+      return;
     }
+
+    const formdata = new FormData();
+    formdata.append("image", selectedFile);
+    console.log("Hi man");
+
+    fetch("https://photo-content-generator.onrender.com/uploadImage", {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
+      mode: "no-cors",
+    })
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
   };
 
-  const logPositions = () => {
-    console.log(positions);
+  useEffect(() => {
+    if (data.length) {
+      const keys = Object.keys(data[0]);
+      ClickSubmit(keys, data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (varContent.length) {
+      console.log(varContent);
+    }
+  }, [varContent]);
+
+  const ClickSubmit = (keys: string[], data: ParsedData[]) => {
+    setDivs((prevDivs) => [
+      ...prevDivs,
+      ...keys.map((key) => ({
+        id: key,
+        position: { x: 0, y: 0 },
+        header: key,
+        size: { width: 80, height: 50 },
+        active: true,
+        fontFamily: "Roboto",
+        fontColor: { r: 0, g: 0, b: 0, a: 1 },
+        info: data.map((d) => d[key]),
+      })),
+    ]);
+  };
+
+  useEffect(() => {
+    if (divs.length) {
+      console.log(divs);
+    }
+  }, [divs]);
+
+  const parentRef = useRef<HTMLDivElement>(null);
+  const handleColorChange = (id: string, color: ColorResult) => {
+    setDivs(
+      divs.map((div) =>
+        div.id === id ? { ...div, fontColor: color.rgb } : div
+      )
+    );
   };
 
   return (
-    <div className=" w-full bg-white h-screen p-4 overflow-hidden">
-      <div className="flex w-full  bg-black ">
-        <div className="flex-1 bg-blue-600">
-
-        <div className="parent" ref={parentRef}>
-        <Draggable
-          bounds="parent"
-          onStop={(e, data) => handleDragStop(e, data, 'child1')}
-        >
-          <div className="child" id="child1">Child 1</div>
-        </Draggable>
-        <Draggable
-          bounds="parent"
-          onStop={(e, data) => handleDragStop(e, data, 'child2')}
-        >
-          <div className="child" id="child2">Child 2</div>
-        </Draggable>
-        
-      </div>
-      <button onClick={logPositions}>Log Positions</button>
+    <div className="w-full bg-white h-screen p-4 ">
+      <div className="flex w-full bg-black">
+        <div className="flex-1 bg-blue-600 ">
+          <div>
+            <div>
+              <input type="file" onChange={handleImgFileChange} />
+              <button onClick={handleFinalSubmit}>Upload</button>
+            </div>
+            <div onClick={handleDataRequest}>SEND DATA REQUEST</div>
+          </div>
+          <div className="parent" ref={parentRef}>
+            {divs.map((div) => {
+              if (div.active) {
+                return (
+                  <DraggableResizableDiv
+                    key={div.id}
+                    id={div.id}
+                    position={div.position}
+                    size={div.size}
+                    onUpdate={updateDiv}
+                    name={div.header}
+                    fontFamily={div.fontFamily}
+                    fontColor={div.fontColor}
+                  />
+                );
+              }
+              return null;
+            })}
+          </div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                {divs.map((div) => (
+                  <TableHead key={div.header}>{div.header}</TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                {divs.map((div) => (
+                  <TableCell key={div.id}>
+                    <button
+                      onClick={() => {
+                        setDivs(
+                          divs.map((d) =>
+                            d.id === div.id ? { ...d, active: !d.active } : d
+                          )
+                        );
+                      }}
+                    >
+                      {String(!div.active)}
+                    </button>
+                  </TableCell>
+                ))}
+              </TableRow>
+              <TableRow>
+                {divs.map((div) => (
+                  <TableCell key={div.id} className="">
+                    <button
+                      className="mr-2 border-2 border-darkblue"
+                      onClick={() => {
+                        setDivs(
+                          divs.map((d) =>
+                            d.id === div.id ? { ...d, fontFamily: "Roboto" } : d
+                          )
+                        );
+                      }}
+                    >
+                      Rob
+                    </button>
+                    <button
+                      className="mr-2 border-2 border-darkblue"
+                      onClick={() => {
+                        setDivs(
+                          divs.map((d) =>
+                            d.id === div.id
+                              ? { ...d, fontFamily: "Poppins" }
+                              : d
+                          )
+                        );
+                      }}
+                    >
+                      Poppins
+                    </button>
+                  </TableCell>
+                ))}
+              </TableRow>
+              <TableRow>
+                {divs.map((div) => (
+                  <TableCell key={div.id}>
+                    <SketchPicker
+                      color={div.fontColor}
+                      onChange={(color) => handleColorChange(div.id, color)}
+                    />
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableBody>
+          </Table>
         </div>
-        <div className="flex-1 bg-red-500 ">
-          <div className=" flex justify-center gap-4 bg-slate-500 py-4">
+        <div className="flex-1 bg-red-500">
+          <div className="flex justify-center gap-4 bg-slate-500 py-4">
             <Input
               type="file"
               accept=".csv"
-              className=" m-2 size-min"
+              className="m-2 size-min"
               onChange={handleFileChange}
-            ></Input>
+            />
             <button onClick={handleSubmit}>Upload</button>
           </div>
-          <div className=" bg-white h-[100vh] overflow-y-scroll p-4">
-            {data.length && (
-              <Table className="">
+          <div className="bg-white h-[100vh] overflow-y-scroll p-4">
+            {data.length > 0 && (
+              <Table>
                 <TableHeader>
                   <TableRow>
                     {Object.keys(data[0]).map((key) => (
